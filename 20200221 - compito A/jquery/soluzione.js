b7877dbeb9747e42b16fe3c2a17af34617c1a325
Samuele-giancarli <samugianca@gmail.com>
2024-01-31 16:51:19 +0100

    first jquery commit, è un esercizio json quindi devo spostare tutto su xampp

--- 20200221 - compito A/jquery/soluzione.js
+++ 20200221 - compito A/jquery/soluzione.js
@@ -5,3 +5,15 @@ function rgb2hex(orig){
      ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
      ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : orig;
 }
+
+$(document).ready(function(){
+    let provvisoria = $('main')
+
+    fetch('sw_a.json').then(response => response.json())
+    .then(InformationArray => {
+        InformationArray.data
+        .forEach(character => {
+            provvisoria.append('<label>${character.id}</label>');
+        })
+    })
+})
